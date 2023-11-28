f29272d782678e2b82c4953fe09a28d821ea9acf
Emmacodec <danielemmanuel7553@gmail.com>
2023-11-28 04:37:40 +0100

    working on hands on practice on NodeJs

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+// static files
+app.use(express.static('public'));
