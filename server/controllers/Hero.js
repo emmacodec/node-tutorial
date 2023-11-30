a831769e221ad6d2823ff2f8cb7415bbd78c8d85
Emmacodec <danielemmanuel7553@gmail.com>
2023-11-30 06:28:11 +0100

    working on the front page

--- server/controllers/Hero.js
+++ server/controllers/Hero.js
@@ -9,7 +9,10 @@ exports.homepage = async (req, res) => {
         description: 'Free NodeJs Notes App.',
     };
 
-    res.render('index', locals);
+    res.render('index', {
+        locals,
+        layout: '../views/layouts/front-page'
+    });
 }
 
 
