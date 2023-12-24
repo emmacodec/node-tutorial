049d688530a785a2313b22058f4b0819d7b2448e
Emmacodec <danielemmanuel7553@gmail.com>
2023-12-24 14:37:02 +0100

    wrote a function that checks for errors

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/dashboard'));
+
+
+// Handle 404 Error
+app.get('*', function(res, req) {
+    //res.status(404).send('404 Page Not Found.')
+    res.status(404).render('404');
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
